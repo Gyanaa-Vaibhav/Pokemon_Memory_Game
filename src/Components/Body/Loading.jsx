241f9cfc1ff813ee3b2427d1702b856b8b925c45
Gyanaa Vaibhav <gynanrudr0@gmail.com>
2024-11-22 16:02:21 +0530

    Added extra Line

--- src/Components/Body/Loading.jsx
+++ src/Components/Body/Loading.jsx
@@ -20,6 +20,7 @@ export default function Loading({ setPlayBackgroundMusic,setIsPlaying }) {
                     useSound('click')
                 }}
                 className='header-title'>Pokemon Memory Game</h1>
+            <p className='header-desc'>Improves Rusty Old Memory</p>
             <p className='header-desc'>Score Point by clicking a different card after each shuffle</p>
             <button
                 style={btnStyle}
